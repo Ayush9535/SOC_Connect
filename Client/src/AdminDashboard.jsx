0c8dd0ad4150b7c6a9ed03d3cd62f30a2e7af2a7
unknown <bhagirath961778@gmail.com>
2024-09-26 15:31:23 +0530

    dashboard files created

---
+++ Client/src/AdminDashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminSideBar from './Components/AdminSideBar'
+function AdminDashboard() {
+  return (
+    <div>
+      <div className="sidebar">
+        <AdminSideBar/>
+      </div>
+      <div className="admin_cntr"></div>
+    </div>
+  )
+}
+
+export default AdminDashboard
---
+++ Client/src/StudentDashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function StudentDashboard() {
+  return (
+    <>
+    <div>StudentDashboard</div>
+    <div>
+    <button>Personal Information</button>
+    <button>Academic Information</button>
+    <button>Student Feedbacks</button>
+    </div>
+    </>
+  )
+}
+
+export default StudentDashboard
